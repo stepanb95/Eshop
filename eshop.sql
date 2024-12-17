b6d3f6dda7bd0e3d89211080d4227c7fb79a1097
stepanb95 <115628037+stepanb95@users.noreply.github.com>
2024-12-17 19:14:16 +0100

    Add files via upload

--- eshop.sql
+++ eshop.sql
@@ -1,66 +1,45 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Počítač: 127.0.0.1
--- Vytvořeno: Úte 10. pro 2024, 13:06
--- Verze serveru: 10.4.32-MariaDB
--- Verze PHP: 8.2.12
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
+-- --------------------------------------------------------
+-- Hostitel:                     localhost
+-- Verze serveru:                8.0.39 - MySQL Community Server - GPL
+-- OS serveru:                   Win64
+-- HeidiSQL Verze:               12.8.0.6908
+-- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
---
--- Databáze: `eshop`
---
 
--- --------------------------------------------------------
+-- Exportování struktury databáze pro
+CREATE DATABASE IF NOT EXISTS `eshop` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `eshop`;
 
---
--- Struktura tabulky `orders`
---
+-- Exportování struktury pro tabulka eshop.orders
+CREATE TABLE IF NOT EXISTS `orders` (
+  `orderId` int DEFAULT NULL,
+  `status` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE `orders` (
-  `Id` int(11) NOT NULL,
-  `Name` int(11) NOT NULL,
-  `Date` date NOT NULL,
-  `Stav` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+-- Exportování dat pro tabulku eshop.orders: ~0 rows (přibližně)
 
--- --------------------------------------------------------
+-- Exportování struktury pro tabulka eshop.products
+CREATE TABLE IF NOT EXISTS `products` (
+  `name` text,
+  `description` text,
+  `price` float DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
---
--- Struktura tabulky `products`
---
+-- Exportování dat pro tabulku eshop.products: ~1 rows (přibližně)
+INSERT INTO `products` (`name`, `description`, `price`) VALUES
+	('test', 'ano', 14);
 
-CREATE TABLE `products` (
-  `Id` int(11) NOT NULL,
-  `Name` text NOT NULL,
-  `Count` int(11) NOT NULL,
-  `Price` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Struktura tabulky `users`
---
-
-CREATE TABLE `users` (
-  `Id` int(11) NOT NULL,
-  `Name` text NOT NULL,
-  `Email` text NOT NULL,
-  `Phone_number` varchar(9) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-COMMIT;
-
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
